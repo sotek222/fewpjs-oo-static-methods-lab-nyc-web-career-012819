10ce093c02af136505d9dbf395a84f65a1591be6
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-03-18 18:58:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.split("")[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-" "']+/g, '');
+  }
+
+  static titleize(string){
+    let words = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let strArr = string.split(" ");
+    let finalString = [];
+
+    strArr.map(function(word){
+      if(words.includes(word) === false){
+        finalString.push(Formatter.capitalize(word));
+      } else {
+        finalString.push(word);
+      }
+    })
+    return Formatter.capitalize(finalString.join(" "));
+  };
+
 }
